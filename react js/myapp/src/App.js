f95a742293a6aab2b36659ea4cc1e1f24a8f5268
alekya <98833881+Anisetti123@users.noreply.github.com>
2022-03-05 21:30:18 +0530

    react.js

--- react js/myapp/src/App.js
+++ react js/myapp/src/App.js
@@ -1,16 +1,30 @@
-import React from "react";
-import Hello from "./components/Hello";
-import MyButton from "./components/MyButton";
+import React, { useState } from "react";
+import Todos from "./components/Todos";
 
-const App = () =>{
-    
+function App() {
+  const[todos,setTodos] = useState([
+  {id:1,text:"Buy Apples",status:true},
+  {id:2,text:"Buy Pineapple",status:false},
+  {id:3,text:"Buy Kiwi",status:true},
+  {id:4,text:"Buy Orange",status:false},
+  {id:5,text:"Buy Watermelon",status:false},
+]);
+   const toggleTodo =(e) =>{
+    console.log (e)
+    setTodos(
+    todos.map((todo)=> todo.id==e.id ? {...todo,status:!e.status} : todo
+    )
+    )
+   };
   return ( 
     <div>
-      <Hello/>
-      <MyButton/>
-      
-    </div> 
-    );
-}
+    <h1>Todos</h1>
+    
+    
+    <Todos list={todos} onToggle={toggleTodo}/>
+    </div>  
+     
+  );
+  }
 
 export default App;
